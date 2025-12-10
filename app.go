71ce6535e47cf0673fc41402444122781a37f291
@tranluuquoctuan1906 <tranluuquoctuan1906@gmail.com>
2025-12-10 15:56:35 +0700

    lession 5

--- app.go
+++ app.go
@@ -5,7 +5,11 @@ import "fmt"
 func main() {
 	var fullName = "John Doe"
 	fmt.Println("Full Name:", fullName)
+
 	var age int
 	fmt.Println("Age:", age)
+
+	phoneNumber := "123-456-7890"
+	fmt.Println("Phone Number:", phoneNumber)
 	randomUser()
 }
