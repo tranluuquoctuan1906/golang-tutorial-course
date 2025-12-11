e137c2400f6e22ff5fc256d5600bbd9a6e8ce3f2
@tranluuquoctuan1906 <tranluuquoctuan1906@gmail.com>
2025-12-11 17:19:03 +0700

    lession 10

--- app.go
+++ app.go
@@ -3,23 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	ten := "John Doe"
-	tuoi := 30
-	chieuCao := 1.75
-	daTotNghiep := true
-	phanTramHocLuc := 85.5
-
-	fmt.Printf("Kiểu dữ liệu của ten là: %T\n", ten)
-	fmt.Printf("Kiểu dữ liệu của tuoi là: %T\n", tuoi)
-	fmt.Printf("Kiểu dữ liệu của tuoi là: %T\n", chieuCao)
-	fmt.Printf("Kiểu dữ liệu của tuoi là: %T\n", daTotNghiep)
-	fmt.Printf("Kiểu dữ liệu của tuoi là: %T\n", phanTramHocLuc)
-
-	fmt.Printf("Giá trị của ten là: %v\n", ten)
-	fmt.Printf("Giá trị của tuoi là: %v\n", tuoi)
-	fmt.Printf("Giá trị của tuoi là: %v\n", chieuCao)
-	fmt.Printf("Giá trị của tuoi là: %v\n", daTotNghiep)
-	fmt.Printf("Giá trị của tuoi là: %v\n", phanTramHocLuc)
-
-	fmt.Printf("My name is %s and my age is %d", ten, tuoi)
+	s1 := 15
+	s2 := 27
+	sum := s1 + s2
+	fmt.Println("Sum:", sum)
+	diff := s1 - s2
+	fmt.Println("Difference:", diff)
+	prod := s1 * s2
+	fmt.Println("Product:", prod)
+	quot := float32(s1) / float32(s2)
+	fmt.Printf("Quotient: %.2f\n", quot)
+	rem := s2 % s1
+	fmt.Println("Remainder:", rem)
 }
