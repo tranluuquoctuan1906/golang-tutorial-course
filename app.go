9b949d93f0fdc02b60165bd5d48d287b0e7b941d
@tranluuquoctuan1906 <tranluuquoctuan1906@gmail.com>
2025-12-11 15:41:52 +0700

    lession 9

--- app.go
+++ app.go
@@ -3,10 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var fullName, address string
-	fmt.Print("Enter your full name: ")
-	fmt.Scanln(&fullName)
-	fmt.Print("Enter your address: ")
-	fmt.Scanln(&address)
-	fmt.Printf("Hello, %s! Your address is %s.\n", fullName, address)
+	ten := "John Doe"
+	tuoi := 30
+	chieuCao := 1.75
+	daTotNghiep := true
+	phanTramHocLuc := 85.5
+
+	fmt.Printf("Kiểu dữ liệu của ten là: %T\n", ten)
+	fmt.Printf("Kiểu dữ liệu của tuoi là: %T\n", tuoi)
+	fmt.Printf("Kiểu dữ liệu của tuoi là: %T\n", chieuCao)
+	fmt.Printf("Kiểu dữ liệu của tuoi là: %T\n", daTotNghiep)
+	fmt.Printf("Kiểu dữ liệu của tuoi là: %T\n", phanTramHocLuc)
+
+	fmt.Printf("Giá trị của ten là: %v\n", ten)
+	fmt.Printf("Giá trị của tuoi là: %v\n", tuoi)
+	fmt.Printf("Giá trị của tuoi là: %v\n", chieuCao)
+	fmt.Printf("Giá trị của tuoi là: %v\n", daTotNghiep)
+	fmt.Printf("Giá trị của tuoi là: %v\n", phanTramHocLuc)
+
+	fmt.Printf("My name is %s and my age is %d", ten, tuoi)
 }
