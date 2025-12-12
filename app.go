1b51c14d4f83cb0158e890c89f76860d5056757b
@tranluuquoctuan1906 <tranluuquoctuan1906@gmail.com>
2025-12-12 10:20:30 +0700

    lession 11

--- app.go
+++ app.go
@@ -5,14 +5,7 @@ import "fmt"
 func main() {
 	s1 := 15
 	s2 := 27
-	sum := s1 + s2
-	fmt.Println("Sum:", sum)
-	diff := s1 - s2
-	fmt.Println("Difference:", diff)
-	prod := s1 * s2
-	fmt.Println("Product:", prod)
-	quot := float32(s1) / float32(s2)
-	fmt.Printf("Quotient: %.2f\n", quot)
-	rem := s2 % s1
-	fmt.Println("Remainder:", rem)
+
+	equal := s1 == s2
+	fmt.Printf("Are s1 and s2 equal? %t\n", equal)
 }
