04afa646e92f7b058d2e871ad37fc6e3d5b8eccb
@tranluuquoctuan1906 <tranluuquoctuan1906@gmail.com>
2025-12-16 16:39:08 +0700

    episode 20

--- app.go
+++ app.go
@@ -1,19 +1,10 @@
 package main
 
-import (
-	"strconv"
-	"strings"
-)
+func sum(a int, b int) int {
+	return a + b
+}
 
 func main() {
-	var xhtml strings.Builder
-	for i := 1; i <= 100; i += 2 {
-		xhtml.WriteString(strconv.Itoa(i))
-		if (i+1)%3 == 0 {
-			xhtml.WriteString("\n")
-		} else if i != 99 {
-			xhtml.WriteString(",")
-		}
-	}
-	println(xhtml.String())
+	result := sum(3, 5)
+	println("The sum is:", result)
 }
