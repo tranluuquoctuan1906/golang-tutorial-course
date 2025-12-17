c37a27adeab63ac7577b6154fb468dcf4312d4d1
@tranluuquoctuan1906 <tranluuquoctuan1906@gmail.com>
2025-12-17 13:41:57 +0700

    episode 21

--- app.go
+++ app.go
@@ -1,10 +1,23 @@
 package main
 
-func sum(a int, b int) int {
-	return a + b
+func sum(a int, b int) (int, int) {
+	sum := a + b
+	diff := a - b
+	return sum, diff
+}
+
+func countdown(n int) {
+	if n <= 0 {
+		println("Done!")
+		return
+	}
+	println(n)
+	countdown(n - 1)
 }
 
 func main() {
-	result := sum(3, 5)
-	println("The sum is:", result)
+	sum, diff := sum(3, 5)
+	println("The sum is:", sum)
+	println("The difference is:", diff)
+	countdown(10)
 }
