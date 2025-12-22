7f31e33fb8ca1f638e57db4df327b58b219ac265
@tranluuquoctuan1906 <tranluuquoctuan1906@gmail.com>
2025-12-22 14:34:00 +0700

    episode 27

--- app.go
+++ app.go
@@ -2,24 +2,9 @@ package main
 
 import "fmt"
 
-func fibonacci(n int) int {
-	if n == 0 {
-		return 0
-	}
-	if n == 1 {
-		return 1
-	}
-	return fibonacci(n-1) + fibonacci(n-2)
-}
-
 func main() {
-	var n int
-	fmt.Printf("Enter a positive integer: ")
-	fmt.Scan(&n)
-	if n < 0 {
-		fmt.Println("Please enter a non-negative integer.")
-		return
-	}
-	result := fibonacci(n)
-	fmt.Printf("Fibonacci(%d) = %d\n", n, result)
+	name := "World"
+	fmt.Printf("%T\n", name)
+	fmt.Printf("%v\n", name)
+	fmt.Printf("%v\n", &name)
 }
