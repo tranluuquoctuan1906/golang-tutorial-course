1e9bb1846e79791ac183d43acea80d2d7fdf15d3
@tranluuquoctuan1906 <tranluuquoctuan1906@gmail.com>
2025-12-22 16:43:23 +0700

    episode 29

--- app.go
+++ app.go
@@ -2,9 +2,19 @@ package main
 
 import "fmt"
 
+func updateVariable(name *string) {
+	*name = "Tran Thi B"
+	fmt.Println("After changing value of name variable")
+	fmt.Printf("Type of name variable: %T\n", name)
+	fmt.Printf("Value of name variable: %v\n", *name)
+	fmt.Printf("Address of name variable: %p\n", name)
+}
+
 func main() {
-	name := "World"
-	fmt.Printf("%T\n", name)
-	fmt.Printf("%v\n", name)
-	fmt.Printf("%v\n", &name)
+	name := "Nguyen Van A"
+	fmt.Println("Information of variable")
+	fmt.Printf("Type of name variable: %T\n", name)
+	fmt.Printf("Value of name variable: %v\n", name)
+	fmt.Printf("Address of name variable: %p\n", &name)
+	updateVariable(&name)
 }
